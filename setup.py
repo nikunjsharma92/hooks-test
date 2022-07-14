a5b0f7989712e8818574ac93519d2cbbe1514aa7
Nikunj Sharma <nikunj.sharma92@gmail.com>
2022-07-14 17:25:03 +0530

    added setup.py:

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='shared',
+      version='0.0.1',
+      description='This is a test hooks package.',
+      author='Nikunj Sharma',
+      author_email='nikunj.sharma92@gmail.com',
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      license='LICENSE.txt',
+    )
